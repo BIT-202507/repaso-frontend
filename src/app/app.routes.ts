d842c634898d650d0c71ff0e295297b27ea88a0d
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-22 20:01:29 -0500

    [+] (Home,Login,Register,PageNotFound).ts: Genera componentes de pagina [+] app.routes.ts: Define todas las rutas necesarias [+] (Header, Footer).ts: Define componentes de layout del site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/pages/home/home';
+import { Login } from './features/pages/login/login';
+import { Register } from './features/pages/register/register';
+import { PageNotFound } from './features/pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // No olvidar que las rutas se poner primero y se definen por su nivel de profundidad
+  { path: 'home', component: Home },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
+  { path: '404', component: PageNotFound },
+  // Las redirecciones van al final
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
+];
